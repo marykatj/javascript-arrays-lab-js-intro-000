d1575e78bfd2751f669c76bbc5c7aa0be03ee43d
Mary-Kate James <marykatj@gmail.com>
2018-04-04 15:33:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,3 +29,13 @@ function prependKitten(name) {
   var newKittens = [name, ...kittens];
   return newKittens;
 }
+
+function removeLastKitten(name) {
+  var newKittens = [kittens.slice(0, kittens.length - 1)];
+  return newKittens;
+}
+
+function removeFirstKitten(name) {
+  var newKittens = [kittens.slice(1)];
+  return newKittens;
+}
